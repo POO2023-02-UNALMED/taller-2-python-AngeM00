6fca0bb246b38980c4bb4f0abd0b8d001e9d98b6
Angelika Moya <angelmb023@gmail.com>
2023-08-18 21:25:36 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,47 @@
+class Auto:
+    cantidadCreados = 0
+    def __init__(self,modelo,precio,asientos,marca,motor,registro):
+        self.modelo = modelo
+        self.precio = precio
+        self.asientos = asientos  # Array de asientos
+        self.marca = marca
+        self.motor = motor  # Atributo de referencia de Motor
+        self.registro = registro
+    
+    def cantidadAsientos(self):
+        return len(self.asientos)
+    
+    def verificarIntegridad(self):
+        verificacion = True
+        for i in self.asientos:
+            if i.registro != self.registro:
+                verificacion = False
+        if self.motor.registro == self.registro and verificacion:
+            print("Auto original")
+        else:
+            print("Las piezas no son originales")
+
+class Asiento:
+    def __init__(self,color,precio,registro):
+        self.color = color
+        self.precio = precio
+        self.registro = registro
+    
+    def cambiarColor(self,color):
+        permitidos = ["rojo", "verde", "amarillo", "negro", "blanco"]
+        if color in permitidos:
+            self.color = color
+
+class Motor:
+    def __init__(self,numeroCilindros,tipo,registro):
+        self.numeroCilindros = numeroCilindros
+        self.tipo = tipo
+        self.registro = registro
+    
+    def cambiarRegistro(self,registro):
+        self.registro = registro
+        
+    def asignarTipo(self,tipo):
+        posibles = ["electrico","gasolina"]
+        if tipo in posibles:
+            self.tipo = tipo
